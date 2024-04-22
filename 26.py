086b4a64a921d5f765026cd006823d59e052b63c
Marufjon Abduqahhorov Mohirjonovich <marufqahhorov001@gmail.com>
2024-04-22 23:15:30 +0500

    26 - dars o'zgarishlar qo'shildi.
    1 - ish.

---
+++ 26.py
@@ -0,0 +1,18 @@
+# XATOLAR BILAN ISHLASH
+
+# Mantiqiy xatolik
+
+#radius = 5
+#pi = 4.14 # 3.14
+#aylana_yuzi = pi*radius**2
+#print(aylana_yuzi)
+
+#son = float(input("Istalgan son kiriting:  "))
+#ildiz = son **1/2 # (1/2) yozilishi kerak edi.
+#print(f"{son} ning ildizi {ildiz} ga teng.")
+
+mevalar = ['olma', 'uzum', 'nok', 'anjir']
+for meva in mevalar:
+    print(meva)
+    print("Dastur tugadi.") # bu yerda ikkinchi print() satr boshida ishlatilishi kerak edi.Yo'qsa u har bir mevadan keyin birga chiqaveradi.
+print("dastur tugadi oka !")
